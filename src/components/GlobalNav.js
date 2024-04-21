1bda8307fdc8e32e9dfee800013689f17217836e
Birhan Aschalew <queenbirsh1719@gmail.com>
2024-04-21 11:40:25 +0300

    m

--- src/components/GlobalNav.js
+++ src/components/GlobalNav.js
@@ -17,7 +17,7 @@ function classNames(...classes) {
 
 export default function GlobalNav() {
   return (
-    <Disclosure as="nav" className="bg-green-50">
+    <Disclosure as="nav" className="bg-green">
       {({ open }) => (
         <>
           <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
