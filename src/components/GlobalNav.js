1fabfc9831cc832b984dbf62309d20323b6e8e4e
Birhan Aschalew <queenbirsh1719@gmail.com>
2024-04-22 16:27:09 +0300

    n

--- src/components/GlobalNav.js
+++ src/components/GlobalNav.js
@@ -3,6 +3,7 @@ import React from "react";
 import { Disclosure } from "@headlessui/react";
 import {  MenuIcon, XIcon } from "@heroicons/react/outline";
 import NavLinkWebsite from "./NavLinkWebsite";
+import { Link } from "react-router-dom";
 
 //import { Link } from "react-router-dom";
 
@@ -16,9 +17,9 @@ const navigation = [
 
 
 
-function classNames(...classes) {
-  return classes.filter(Boolean).join(" ");
-}
+// function classNames(...classes) {
+//   return classes.filter(Boolean).join(" ");
+// }
 
 
 export default function GlobalNav() {
@@ -143,20 +144,25 @@ export default function GlobalNav() {
           <Disclosure.Panel className="sm:hidden">
             <div className="px-2 pt-2 pb-3 space-y-1">
               {navigation.map((item) => (
-                <Disclosure.Button
-                  key={item.name}
-                  as="a"
-                  href={item.href}
-                  className={classNames(
-                    item.current
-                      ? "bg-green-900 text-white"
-                      : "text-green-900 hover:bg-green-700 hover:text-white",
-                    "block px-3 py-2 rounded-md text-base font-medium"
-                  )}
-                  aria-current={item.current ? "page" : undefined}
-                >
+                <Link
+                key={item.name}
+                to={item.href}>
                   {item.name}
-                </Disclosure.Button>
+                </Link>
+                // <Disclosure.Button
+                //   key={item.name}
+                //   as="a"
+                //   href={item.href}
+                //   className={classNames(
+                //     item.current
+                //       ? "bg-green-900 text-white"
+                //       : "text-green-900 hover:bg-green-700 hover:text-white",
+                //     "block px-3 py-2 rounded-md text-base font-medium"
+                //   )}
+                //   aria-current={item.current ? "page" : undefined}
+                // >
+                //   {item.name}
+                // </Disclosure.Button>
               ))}
             </div>
           </Disclosure.Panel>
